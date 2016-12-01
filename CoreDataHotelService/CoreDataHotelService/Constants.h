1ead5ea4fcc4683edd402ce02c8a1dbaa0c753b9
Erica Winberry <elaewin@gmail.com>
2016-12-01 14:23:16 -0800

    added Constants.h to gitignore.

---
+++ CoreDataHotelService/CoreDataHotelService/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  CoreDataHotelService
+//
+//  Created by Erica Winberry on 12/1/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
