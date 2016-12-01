683df84a7fdb60f23b99bd32d04b9dba6dc36ab8
Erica Winberry <elaewin@gmail.com>
2016-11-30 23:05:40 -0800

    Booking working to save new reservation, but not making that room unavailable.

--- CoreDataHotelService/CoreDataHotelService/ReservationService.m
+++ CoreDataHotelService/CoreDataHotelService/ReservationService.m
@@ -11,8 +11,48 @@
 @implementation ReservationService
 
 // get available rooms
-//+(NSFetchedResultsController *)getAvailableRoomsFor:(NSDate *)startDate through:(NSDate *)endDate {
-//}
++(NSFetchedResultsController *)getAvailableRoomsFor:(NSDate *)startDate through:(NSDate *)endDate forFRC:(NSFetchedResultsController *)fetchedResultsController {
+
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+    NSManagedObjectContext *context = appDelegate.persistentContainer.viewContext;
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Reservation"];
+    
+    // Get a list of all rooms WITH reservations.
+    request.predicate = [NSPredicate predicateWithFormat:@"startDate <= %@ AND endDate >= %@", startDate, endDate];
+    
+    NSError *requestError;
+    NSArray *results = [context executeFetchRequest:request error:&requestError];
+    if (requestError) {
+        NSLog(@"Error with reservation fetch request.");
+        return nil;
+    }
+
+    // Add the unavailable rooms to a mutable array.
+    NSMutableArray *unavailableRooms = [[NSMutableArray alloc]init];
+    for (Reservation *reservation in results) {
+        [unavailableRooms addObject:reservation.room];
+    }
+    
+    // Now fetch all of the rooms, and use the predicate to remove the rooms that are already reserved.
+    NSFetchRequest *roomRequest = [NSFetchRequest fetchRequestWithEntityName:@"Room"];
+    roomRequest.predicate = [NSPredicate predicateWithFormat:@"NOT self IN %@", unavailableRooms];
+    
+    // Sort rooms by hotel, then sort by room number
+    roomRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"hotel.name" ascending:YES], [NSSortDescriptor sortDescriptorWithKey:@"roomNumber" ascending:YES]];
+    
+    NSError *roomRequestError;
+    
+    fetchedResultsController = [[NSFetchedResultsController alloc]initWithFetchRequest:roomRequest managedObjectContext:context sectionNameKeyPath:@"hotel.name" cacheName:nil];
+    
+    [fetchedResultsController performFetch:&roomRequestError];
+    
+    if (roomRequestError) {
+        NSLog(@"There was an error requesting available rooms.");
+    }
+    
+    return fetchedResultsController;
+}
 
 // book a reservation
 +(BOOL)bookAReservationFor:(NSDate *)startDate through:(NSDate *)endDate atHotel:(Hotel *)hotel inRoom:(Room *)room withFirstName:(NSString *)firstName andLastName:(NSString *)lastName atEmail:(NSString *)email {
